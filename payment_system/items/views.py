f427c233efbe52f26f36abae649bacc8a184d396
lenger1117 <lenger1117@yandex.ru>
2025-02-27 15:52:28 +0300

    Add view+urls

--- payment_system/items/views.py
+++ payment_system/items/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import stripe
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Item
 
-# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+def item_detail(request, id):
+    item = get_object_or_404(Item, id=id)
+    return render(request, 'items/item_detail.html', {'item': item})
+
+def buy_item(request, id):
+    item = get_object_or_404(Item, id=id)
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price_data': {
+                'currency': 'rub',
+                'product_data': {
+                    'name': item.name,
+                },
+                'unit_amount': int(item.price * 100),
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url='http://localhost:8000/success/',
+        cancel_url='http://localhost:8000/cancel/',
+    )
+    return JsonResponse({'session_id': session.id})
